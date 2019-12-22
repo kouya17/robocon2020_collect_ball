ea27760f1c28b53945f3cc7d49097a596f453305
kouya17 <17383696+kouya17@users.noreply.github.com>
2019-12-22 10:38:27 +0900

    Fix: 赤色領域読み取りテスト追加

--- imageProcessing.py
+++ imageProcessing.py
@@ -27,7 +27,7 @@ class ImageProcessing:
     FIELD_AND_WALL_HSV_RANGE_MIN = [25, 70, 0]
     FIELD_AND_WALL_RANGE_MAX = [120, 255, 255]
 
-    CAMERA_CENTER_CX_T = 220
+    CAMERA_CENTER_CX_T = 240
     CAMERA_CENTER_CY_T = 240
     CAMERA_CENTER_CX = CAMERA_CENTER_CY_T
     CAMERA_CENTER_CY = CAMERA_CENTER_CX_T
@@ -46,6 +46,54 @@ class ImageProcessing:
         # テーブルの読み込み
         # TODO: テーブル情報は仮。csvファイルから読み込むようにする
         self.GOAL_DISTANCE_TABLE = range(241)
+    
+    # @brief 指定色の最大領域を検知する
+    # @param hsv_img HSV変換後の処理対象画像
+    # @param color_name 検知する色の名前
+    # @detail
+    def colorDetect2(self, hsv_img, color_name):
+        mask_0 = np.zeros((480, 480, 1), np.uint8)
+        cv2.circle(mask_0, (self.CAMERA_CENTER_CX_T, self.CAMERA_CENTER_CY_T), self.CAMERA_RANGE_R, 255, thickness=-1)
+
+        #if color_name == 'RED':
+        # 赤色のHSVの値域1
+        hsv_range_min = np.array([0, 64, 0])
+        hsv_range_max = np.array([30, 255, 255])
+        mask1 = cv2.inRange(hsv_img, np.array(hsv_range_min), np.array(hsv_range_max))
+
+        # 赤色のHSVの値域2
+        hsv_range_min = np.array([150, 64, 0])
+        hsv_range_max = np.array([179, 255, 255])
+        mask2 = cv2.inRange(hsv_img, np.array(hsv_range_min), np.array(hsv_range_max))
+        mask = mask1 + mask2
+        # mask = cv2.inRange(hsv_img, np.array(hsv_range_min), np.array(hsv_range_max))
+        mask = cv2.bitwise_and(mask, mask, mask=mask_0)
+
+        if self.DEBUG_IMSHOW == self.ENABLE:
+            #cv2.imshow('Mask' + color_name, mask.transpose((1, 0)))
+            cv2.imshow('Mask' + color_name, mask)
+
+        _, contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        convex_hull_list = []
+        for contour in contours:
+            approx = cv2.convexHull(contour)
+
+            M = cv2.moments(approx)
+            convex_hull_list.append({'approx': approx, 'moment': M})
+
+        if len(convex_hull_list) > 0:
+            max_convex_hull = max(convex_hull_list, key=(lambda x: x['moment']['m00']))
+            if max_convex_hull['moment']['m00'] > 0:
+                area_size = max_convex_hull['moment']['m00']
+
+                cx = int(max_convex_hull['moment']['m10'] / max_convex_hull['moment']['m00'])
+                cy = int(max_convex_hull['moment']['m01'] / max_convex_hull['moment']['m00'])
+
+                return cx, cy, area_size, max_convex_hull['approx']
+            return -1, -1, 0.0, []
+        else:
+            return -1, -1, 0.0, []
 
     # @brief 指定色の最大領域を検知する
     # @param hsv_img HSV変換後の処理対象画像
@@ -172,6 +220,7 @@ class ImageProcessing:
                  (0, 0, 255), thickness=1)
         cv2.line(frame, (0, self.CAMERA_CENTER_CY_T), (480, self.CAMERA_CENTER_CY_T),
                  (0, 0, 255), thickness=1)
+        '''
         # 黄色領域の検知
         yellow_cx_t, yellow_cy_t, yellow_area_size, yellow_convex = self.colorDetect(hsv_img, self.YELLOW_HSV_RANGE_MIN,
                                                                                      self.YELLOW_HSV_RANGE_MAX,
@@ -191,8 +240,15 @@ class ImageProcessing:
         if field_cx_t > -1:
             cv2.drawContours(frame, [field_convex], 0, (0, 255, 0), thickness=1)
             self.draw_marker(frame, field_cx_t, field_cy_t, (0, 128, 0))
+        '''
 
-        DEBUG('Yellow :' + str(yellow_area_size).rjust(8), 'Blue :' + str(blue_area_size).rjust(8))
+        # 赤色領域の検知
+        red_cx, red_cy, red_area_size, red_convex = self.colorDetect2(hsv_img, 'Red')
+        if red_cx_t > -1:
+            self.draw_marker(frame, red_cx, red_cy, (255, 30, 30))
+
+        #DEBUG('Yellow :' + str(yellow_area_size).rjust(8), 'Blue :' + str(blue_area_size).rjust(8))
+        DEBUG('Red :' + str(red_area_size).rjust(8))
 
         # 画角の前後左右と画像表示の上下左右を揃えるためにx座標とy座標を交換する。
         '''
@@ -202,22 +258,27 @@ class ImageProcessing:
         blue_cy = blue_cx_t
         field_cx = field_cy_t
         field_cy = field_cx_t
-        '''
+
         yellow_cx = yellow_cx_t
         yellow_cy = yellow_cy_t
         blue_cx = blue_cx_t
         blue_cy = blue_cy_t
         field_cx = field_cx_t
         field_cy = field_cy_t
+        '''
 
+        '''
         yellow_goal_angle, yellow_goal_distance = self.calcBallDirection(yellow_cx, yellow_cy, yellow_area_size)
         blue_goal_angle, blue_goal_distance = self.calcBallDirection(blue_cx, blue_cy, blue_area_size)
         field_center_angle, field_center_distance = self.calcBallDirection(field_cx, field_cy, field_area_size)
         DEBUG('Yellow : Angle=' + str(yellow_goal_angle).rjust(4), 'Dis=' + str(yellow_goal_distance).rjust(3),
               'Blue : Angle=' + str(blue_goal_angle).rjust(4), 'Dis=' + str(blue_goal_distance).rjust(3),
               'field : Angle=' + str(field_center_angle).rjust(4), 'Dis=' + str(field_center_distance).rjust(3),)
+        '''
+        red_ball_angle, red_ball_distance = self.calcBallDirection(red_cx, red_cy, red_area_size)
 
-        return yellow_goal_angle, yellow_goal_distance, blue_goal_angle, blue_goal_distance, field_center_angle, field_center_distance
+        #return yellow_goal_angle, yellow_goal_distance, blue_goal_angle, blue_goal_distance, field_center_angle, field_center_distance
+        return red_ball_angle, red_ball_distance
 
     # @brief 画像処理のmain処理
     # @param shmem 共有メモリ
@@ -233,7 +294,8 @@ class ImageProcessing:
                     # 画像を取得し、stream.arrayにRGBの順で映像データを格納
                     camera.capture(stream, 'bgr', use_video_port=True)
 
-                    yellow_goal_angle, yellow_goal_distance, blue_goal_angle, blue_goal_distance, field_center_angle, field_center_distance = self.imageProcessingFrame(stream.array, shmem)
+                    #yellow_goal_angle, yellow_goal_distance, blue_goal_angle, blue_goal_distance, field_center_angle, field_center_distance = self.imageProcessingFrame(stream.array, shmem)
+                    red_ball_angle, red_ball_distance = self.imageProcessingFrame(stream.array, shmem)
 
                     # 結果表示
                     # 画角の前後左右と画像表示の上下左右を揃えるために画像を転置する。
@@ -242,13 +304,14 @@ class ImageProcessing:
                         #cv2.imshow('Frame', stream.array.transpose((1, 0, 2)))
                         cv2.imshow('Frame', stream.array)
                         cv2.moveWindow('Frame', 0, 30)
-                        cv2.moveWindow('MaskYellow', 482, 30)
-                        cv2.moveWindow('MaskBlue', 964, 30)
-                        cv2.moveWindow('MaskWall', 482, 502)
+                        cv2.moveWindow('MaskRed', 482, 30)
+                        #cv2.moveWindow('MaskYellow', 482, 30)
+                        #cv2.moveWindow('MaskBlue', 964, 30)
+                        #cv2.moveWindow('MaskWall', 482, 502)
               
                     # 共有メモリに書き込む
-                    shmem.ballAngle = int(blue_goal_angle)
-                    shmem.ballDis = int(blue_goal_distance)
+                    shmem.ballAngle = int(red_ball_angle)
+                    shmem.ballDis = int(red_ball_distance)
 
                     # "q"でウィンドウを閉じる
                     if cv2.waitKey(1) & 0xFF == ord("q"):
